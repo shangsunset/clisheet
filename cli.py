d8f7618c79f4482cea5f5cba40c9ff4115b08827
Yeshen Shang <shangsunset@gmail.com>
2015-03-07 22:26:37 -0500

    checkin and checkout

--- cli.py
+++ cli.py
@@ -12,23 +12,42 @@ session = Session()
 
 
 @click.command()
+@click.option('--sheet', '-s', multiple=True, default=1,
+                help='display a timesheet. take timesheet id as parameter.')
 @click.argument('arg', required=False)
-def main(arg):
+def main(arg, sheet):
 
-    if arg == 'new':
-        add_new_sheet()
-    if arg == 'ls':
-        list_all_sheets()
-    if arg == 'in':
-        check_in()
-
-
-def add_new_sheet():
     archive = session.query(TimesheetArchive).filter(TimesheetArchive.name=='archive').first()
-    sheets = session.query(Timesheet).all()
     if not archive:
         archive = TimesheetArchive()
         session.add(archive)
+        session.commit()
+
+    if arg == 'new':
+        add_new_sheet(archive)
+    if arg == 'ls':
+        list_all_sheets(archive)
+    if arg == 'in':
+        check_in(archive)
+    if arg == 'out':
+        check_out()
+
+    show_sheet(sheet)
+
+
+#TODO
+def show_sheet(sheet_id):
+    # sheets_count = session.query(Timesheet).count()
+    sheet = session.query(Timesheet).get(sheet_id)
+    if sheet:
+        print 'showing %s' % sheet.name
+
+    else:
+        print 'The sheet you are looking for doesnt exist'
+
+
+def add_new_sheet(archive):
+    sheets = session.query(Timesheet).all()
 
     res = session.query(Timesheet).filter(
             datetime.now() - Timesheet.created_date < timedelta(days=1))
@@ -38,13 +57,13 @@ def add_new_sheet():
             print 'Sigh...You already created a timesheet today...'
         else:
             archive.timesheets.append(Timesheet())
-            session.commit()
     else:
         archive.timesheets.append(Timesheet())
-        session.commit()
+
+    session.commit()
 
 
-def list_all_sheets():
+def list_all_sheets(archive):
     res = session.query(Timesheet).all()
     if res:
         print '+---------------------------------------------------------------+'
@@ -59,21 +78,43 @@ def list_all_sheets():
 
 
 
-def check_in(current_time=datetime.now()):
+def check_in(archive, current_time=datetime.now()):
 
     res = session.query(Timesheet).filter(
-            datetime.now() - Timesheet.created_date < timedelta(days=1))
+            datetime.now() - Timesheet.created_date < timedelta(days=1)).first()
     if res:
-        res.entries.append(Entry(checkin_time=current_time))
-        session.commit()
+        entry = session.query(Entry).filter(
+                datetime.now() - Entry.date < timedelta(days=1)).first()
+
+        if entry and not entry.checkout_time:
+            print 'You havent checked out from the last session. ' \
+                    'I think this is a repeated action'
+
+        elif entry and entry.checkout_time:
+            entry.checkin_time = current_time
+
+        else:
+            res.entries.append(Entry(checkin_time=current_time))
     else:
-        archive = session.query(TimesheetArchive).filter(TimesheetArchive.name=='archive').first()
         sheet = Timesheet()
         archive.timesheets.append(sheet)
         sheet.entries.append(Entry(checkin_time=current_time))
-        session.commit()
+
+    session.commit()
 
 
+
+def check_out(current_time=datetime.now()):
+    entry = session.query(Entry).filter(
+            datetime.now() - Entry.date < timedelta(days=1)).first()
+    if entry:
+        entry.checkout_time = current_time
+        entry.hours = ((entry.checkout_time - entry.checkin_time).seconds)/(60*60)
+
+    else:
+        print 'You havent checked in yet son!'
+
+    session.commit()
 
 
 
@@ -86,3 +127,4 @@ def check_in(current_time=datetime.now()):
     # time_in = datetime.strptime(checkin_time, "%H:%M:%S")
     # time_out = datetime.strptime(checkout_time, "%H:%M:%S")
     # working_time = time_out - time_in
+
